b78515bffe8cb3ec445042a774b2bb7bfb1994c7
johns7kr <johns7kr@mail.uc.edu>
2022-09-18 17:30:42 -0400

    powershell files

---
+++ powershell/Body.ps1
@@ -0,0 +1,27 @@
+﻿
+function getIP{
+    (get-netipaddress).ipv4address | Select-String "192*"
+}
+
+function getVersion{
+ (Get-Host).version | Select-String "5*"
+}
+
+function host{
+ (Get-ComputerInfo).CsDNSHostName
+}
+
+function date{
+ Get-Date
+}
+
+$User = $env:USERNAME
+$Ver = getVersion
+$IP = getIP
+$Host1 = host
+$Date = date
+
+$Body = write-output("This machine's IP is $IP. User is $User. Host name is $Host1. Powershell Version $Ver. Today's Date is $Date") | out-file -filepath C:\Body.txt
+
+$Body
+notepad C:\Body.txt
