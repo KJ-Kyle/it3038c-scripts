a83213585a61e239a3d25c241a1a811f2faf68e2
KJKY <johns7kr@mail.uc.edu>
2022-11-15 15:32:34 -0500

    fixing token

--- python/test.py
+++ python/test.py
@@ -4,7 +4,7 @@ import requests
 print('Please enter your zip code:')
 zip = input()
 
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=%s,us&appid=5a1a8d925b0ee952d5259d0f50efa6d5' % zip)
+r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=%s,us&appid=CHANGETOTOKEN' % zip)
 data=r.json()
 print(data)
 print(data['weather'][0]['description'])
